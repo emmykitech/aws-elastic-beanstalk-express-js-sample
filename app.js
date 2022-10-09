482b2a62e636df9b84d1f181014544a21e3e0aaa
KiTech <emmakagbabuwe@gmail.com>
2022-10-09 20:34:19 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('DevOps guys for everyone'));
+app.get('/', (req, res) => res.send('DevOps guys, you might as well follow me on twitter!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
